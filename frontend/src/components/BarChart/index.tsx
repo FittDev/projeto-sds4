89674bdf5d08728d2c8da6d5c082b4c22b32d6ba
Emerson Borges <borges.emerson.dba@gmail.com>
2021-06-16 19:28:45 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -60,18 +60,7 @@ const BarChart = () => {
         },
     };
     
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
-        return (            
+    return (            
             <Chart
               options={{...options, xaxis:chartData.labels}}
               series={chartData.series}
